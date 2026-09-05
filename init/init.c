cfbb12fb4731be01c0557675ff599f337c0b214a
freekysachin <freekysachin06@gmail.com>
2026-09-05 23:15:33 +0530

    implemented emergency shell to prevent kernel panic

--- init/init.c
+++ init/init.c
@@ -4,25 +4,37 @@
 #include <sys/mount.h>
 #include <unistd.h>
 
+static void emergency_shell(){
+  fprintf(stderr, "\nemergency shell...\n");
+  char *const args[] = {"/bin/sh", NULL};
+  execve(args[0], args, NULL);
+
+  fprintf(stderr, "Cannot exec emergency shell: %s\n", strerror(errno)); // if execve itself fails
+  for(;;) {
+    pause();
+  }
+}
+
 int main() {
   // mount the our rootfs directories for initrmfs
   int procMount = mount("proc", "/proc", "proc", 0, NULL);
   if (procMount != 0) {
-    printf("Unable to mount proc/");
+    fprintf(stderr, "Unable to mount /proc: %s\n", strerror(errno)); // Print the error message to stderr
 
-    return 0;
+    // Do not return as this is our PID 1 process and if we return, the kernel will panic and we will not be able to debug the issue
+    emergency_shell();
   }
 
   int sysMount = mount("sysfs", "/sys", "sysfs", 0, NULL);
   if (sysMount != 0) {
-    printf("Unable to mount sys/");
+    fprintf(stderr, "Unable to mount /sys: %s\n", strerror(errno));
+    emergency_shell();
   }
 
   int devMount = mount("devtmpfs/", "/dev", "devtmpfs", 0, NULL);
   if (devMount != 0) {
-    printf("Unable to mount dev/");
-
-    return errno;
+    fprintf(stderr, "Unable to mount /dev: %s\n", strerror(errno));
+    emergency_shell();
   }
 
   printf("\n==============\nMount Successful\n==============\n");
